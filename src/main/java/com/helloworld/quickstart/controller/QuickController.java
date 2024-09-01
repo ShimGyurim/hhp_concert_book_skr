2a7954aa2865affc0fc9d53e965da34cdb5f0f95
unknown <pc95@naver.com>
2024-09-01 21:10:23 +0900

    첫 커밋

---
+++ src/main/java/com/helloworld/quickstart/controller/QuickController.java
@@ -0,0 +1,22 @@
+package com.helloworld.quickstart.controller;
+
+import lombok.extern.java.Log;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class QuickController {
+
+    @GetMapping("/dummy")
+    public String dummy() {
+        log.info("dummy");
+        return "{}";
+    }
+    @GetMapping("/dummy2")
+    public String dummy2() {
+        return "dummy2";
+    }
+}
