0c67ec961556774bda3611d2203347f33de8040a
skr1995 <pc95@naver.com>
2024-10-19 12:43:40 +0900

    Constant 위치 변경 (도메인 계층 한정 사용)

--- src/main/java/io/hhplus/concertbook/domain/constant/WaitQueueConstant.java
+++ src/main/java/io/hhplus/concertbook/domain/constant/WaitQueueConstant.java
@@ -1,5 +1,5 @@
 package io.hhplus.concertbook.domain.constant;
 
-public class QueueConstant {
+public class WaitQueueConstant {
     public static final int WAIT_MINUTE = 5; //대기열 PROCESS 대기시간, 임시 좌석 배정시간
 }
