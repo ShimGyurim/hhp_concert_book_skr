8edaa79475ab8250f60078f0eedfb29a9d88e809
skr1995 <pc95@naver.com>
2024-10-23 00:44:51 +0900

    필터 로깅 - 초안

---
+++ src/main/java/io/hhplus/concertbook/common/config/FilterConfig.java
@@ -0,0 +1,17 @@
+package io.hhplus.concertbook.common.config;
+
+import io.hhplus.concertbook.common.filter.LoggingFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfig {
+    @Bean
+    public FilterRegistrationBean<LoggingFilter> loggingFilter() {
+        FilterRegistrationBean<LoggingFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new LoggingFilter());
+        registrationBean.addUrlPatterns("/*"); // 모든 URL 패턴에 대해 필터 적용
+        return registrationBean;
+    }
+}
