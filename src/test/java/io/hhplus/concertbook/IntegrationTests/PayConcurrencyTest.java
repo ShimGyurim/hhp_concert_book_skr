8fcf3097055ec7ce2bda249bd1a775de8141102a
skr1995 <pc95@naver.com>
2024-10-31 23:23:40 +0900

    refactor: 파일 이름변경 PaymentConcurrencyTest -> PayConcurrencyTest

--- src/test/java/io/hhplus/concertbook/IntegrationTests/PayConcurrencyTest.java
+++ src/test/java/io/hhplus/concertbook/IntegrationTests/PayConcurrencyTest.java
@@ -27,7 +27,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(classes = ConcertBookApp.class)
-public class PaymentConcurrencyTest {
+public class PayConcurrencyTest {
 
     @Autowired
     private ConcertService concertService;
